fe34b51a963c4eaa74fe5caa9b8f98047f4710ff
nhat162003 <85332527+nhat162003@users.noreply.github.com>
2021-06-04 14:12:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Ku à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    Lạc vào web anh
+    Ấn tiếp đi ko có virut đâu`,
+    btnIntro: 'Ấn vào để nhận vr',
+    title: 'Chào anh Nhất đz đi 😙',
+    desc: 'Chào như thế nào ? ',
+    btnYes: 'Chào Đại Ca <33',
+    btnNo: 'Có cl đ*t con bà m :)',
+    question:'T biết m định ấn đâu mà ',
+    btnReply: 'Nói gì nói đi',
+    reply: 'Rồi, nói tiếp đi',
+    mess: 'Vô văn hóa !',
+    messDesc: 'Tối cần gánh team không ?.',
+    btnAccept: 'Có Anh ơi',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
