ff1529885e3fe6b306e9cdf06f05cc648f1ac1e3
nhat162003 <85332527+nhat162003@users.noreply.github.com>
2021-06-04 14:23:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Ê!',
     introDesc: `Nhóc con
-    Oke`,
+    Ấn vào xem dính virut ko`,
     btnIntro: 'hihi',
-    title: 'N o n on n on Non 😙',
+    title: 'Dám Ấn cơ ghê',
     desc: 'Chào anh Nhất đz đê ',
     btnYes: 'Vâng ạ <33',
     btnNo: 'Có cl đ*t con bà m :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại chào anh <3',
+    question:'Giờ nói gì đi',
     btnReply: 'Gửi cho anh kk',
-    reply: 'Chào thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Đỉnh ko kk',
-    messDesc: 'Tối nay 7h anh qua đón chú đi cách ly nhá.',
+    reply: 'Gì nữa',
+    mess: 'Vô văn HÓa',
+    messDesc: 'Tối anh gánh ok không.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
