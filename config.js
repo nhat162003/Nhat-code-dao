f82b19d7fc46b1f6828e6aae77d0a907548f8956
nhat162003 <85332527+nhat162003@users.noreply.github.com>
2022-06-07 21:15:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'hello e Toàn!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Bỏ elsu đi 
+    Không nát mùa hè của các e nhỏ`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Ai chơi elsu hay nhất😙',
+    desc: 'Là ai ???? ',
+    btnYes: 'Vẫn cứ là anh Nhất <33',
+    btnNo: 'Đéo phải mày',
+    question:'Ok anh biết mà',
+    btnReply: 'đcm bàn phím óc lồn à đcm nó nữa chứ ..............',
+    reply: 'T chịu à nha haha',
+    mess: 'hey hey hey hey hey 😘😘',
+    messDesc: 'Ib vs anh để thê, giáo án elsu nhé',
+    btnAccept: 'đéo nhé ',
+    messLink: 'http://fb.com' //https://www.facebook.com/profile.php?id=100064698457174
 }
